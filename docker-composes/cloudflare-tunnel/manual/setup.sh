7463054f6e8547dbb0257b454ec2b5ea683cafc4
Nam Vice WSL Ubuntu <trinh.lehainam@vice.co.jp>
2024-08-02 02:18:52 +0900

    Update cloudflare-tunnel configuration to work with Nginx Proxy Manager ssl.

---
+++ docker-composes/cloudflare-tunnel/manual/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# NOTE: you may need change folder permission of <PATH_TO_CONFIG_FOLDER> to 777 for Docker can write to it
+# https://github.com/aeleos/cloudflared/issues/20
+
+# Login
+docker run -it --rm -v <PATH_TO_CONFIG_FOLDER>:/home/nonroot/.cloudflared/ cloudflare/cloudflared:2024.6.1-amd64 tunnel login 
+
+# Create tunnel
+docker run -it --rm -v <PATH_TO_CONFIG_FOLDER>:/home/nonroot/.cloudflared/ cloudflare/cloudflared:2024.6.1-amd64 tunnel create TUNNEL_NAME
