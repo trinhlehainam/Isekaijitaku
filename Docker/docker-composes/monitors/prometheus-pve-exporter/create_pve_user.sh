1544bc4bda195e45fa675521a94224b0269543e6
Nam Vice WSL Ubuntu <trinh.lehainam@vice.co.jp>
2024-08-11 14:01:03 +0900

    Migrate prometheus and grafana to netdata.

---
+++ Docker/docker-composes/monitors/prometheus-pve-exporter/create_pve_user.sh
@@ -0,0 +1,7 @@
+# https://github.com/influxdata/telegraf/blob/release-1.31/plugins/inputs/proxmox/README.md#permissions
+
+pveum user add pve-exporter@pve
+pveum acl modify / -role PVEAuditor -user pve-exporter@pve
+## Create a token with the PVEAuditor role
+pveum user token add pve-exporter@pve monitoring -privsep 1
+pveum acl modify / -role PVEAuditor -token 'pve-exporter@pve!monitoring'
