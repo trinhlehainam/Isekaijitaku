513e63fbe4bcfce69837f5c1d0c91083a84ae665
Nam Vice WSL Ubuntu <trinh.lehainam@vice.co.jp>
2024-08-17 17:32:44 +0900

    Reconfig ip range for public services docker composes.

--- Docker/openwrt_macvlan/setup_openwrt_macvlan.sh
+++ Docker/openwrt_macvlan/setup_openwrt_macvlan.sh
@@ -7,3 +7,8 @@ docker network create -d macvlan -o parent=eth0 \
   --ip-range 10.10.10.0/27 \
   --aux-address "host=10.10.10.3" \
   openwrt_vlan
+
+ip link add openwrt-shim link eth0 type macvlan mode bridge
+ip addr add 10.10.10.3/32 dev openwrt-shim
+ip link set openwrt-shim up
+ip route add 10.10.10.0/27 dev openwrt-shim
