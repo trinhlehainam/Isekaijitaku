44bef2777def565ea4dbc8ab8769f072637f1e96
Nam Vice WSL Ubuntu <trinh.lehainam@vice.co.jp>
2024-08-19 00:55:54 +0900

    Add NetworkManager autoconfig to create bridge that allow host to access containers macvlan network.

--- Docker/openwrt_macvlan/setup_openwrt_macvlan.sh
+++ Docker/openwrt_macvlan/setup_openwrt_macvlan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # https://blog.oddbit.com/post/2018-03-12-using-docker-macvlan-networks/
+
 docker network create -d macvlan -o parent=eth0 \
   --subnet 10.10.10.0/24 \
   --gateway 10.10.10.1 \
