2955a7b4d313712e91a48881491eab6411b3b384
NamVice <trinhlehainam@protonmail.com>
2024-12-04 15:27:44 +0900

    Update vaultwarden using Postgres database.

--- Docker/manifests/vaultwarden/admin_password_generate.sh
+++ Docker/manifests/vaultwarden/admin_password_generate.sh
@@ -1,1 +1,1 @@
-cat admin_password | xargs echo -n | argon2 "$(openssl rand -base64 32)" -e -id -k 19456 -t 2 -p 1 > admin_password_hash
+cat admin_password | xargs echo -n | argon2 "$(openssl rand -base64 32)" -e -id -k 19456 -t 2 -p 1 > ./secrets/admin_token
